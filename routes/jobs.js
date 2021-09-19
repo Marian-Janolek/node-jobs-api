b15290b3f2f2509d5e22d0ecbde088e02e07933c
Marian-Janolek <m.janolek@gmail.com>
2021-09-19 10:59:17 +0200

    initial commit

---
+++ routes/jobs.js
@@ -0,0 +1,14 @@
+const {
+  getAllJobs,
+  getJob,
+  updateJob,
+  createJob,
+  deleteJob,
+} = require('../controllers/jobs');
+const express = require('express');
+const router = express.Router();
+
+router.route('/').post(createJob).get(getAllJobs);
+router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+
+module.exports = router;
